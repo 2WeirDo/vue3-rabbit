f8f302844d64d67691fd624ac77db461ed3cfa52
zweirdo <1606403438@qq.com>
2023-07-19 00:28:55 +0800

    layout静态结构搭建

--- src/App.vue
+++ src/App.vue
@@ -5,15 +5,11 @@
 <template>
      <!-- 一级路由出口组件 -->
      <router-view></router-view>
-     <div class="test">
-      自动导入
-     </div>
+
 </template>
 
 <style scoped lang="scss">
-.test{
-  color: $priceColor;
-}
+
 header {
   line-height: 1.5;
   max-height: 100vh;
--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -1,8 +1,12 @@
+<script setup>
+import LayoutNav from './components/LayoutNav.vue'
+import LayoutHeader from './components/LayoutHeader.vue'
+import LayoutFooter from './components/LayoutFooter.vue'
+</script>
+
 <template>
-    <div>我是方法
-        <!-- 二级路由出口 -->
-        <router-view></router-view>
-    </div>
-    
-    
-</template>
+  <LayoutNav />
+  <LayoutHeader />
+  <RouterView />
+  <LayoutFooter />
+</template> 
