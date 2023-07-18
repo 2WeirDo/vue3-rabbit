a93818d6ac300fdb00039e94fe5f9dcb02f2051c
zweirdo <1606403438@qq.com>
2023-07-19 01:18:39 +0800

    渲染一级导航列表

--- src/views/Layout/components/LayoutHeader.vue
+++ src/views/Layout/components/LayoutHeader.vue
@@ -1,5 +1,18 @@
 <script setup>
 
+import {getCategoryAPI} from '@/apis/layout'
+import { onMounted, ref } from 'vue';
+
+const categoryList = ref([]);
+const getCategory = async() => {
+  const res = await getCategoryAPI();
+  console.log(res);
+  categoryList.value = res.result;
+}
+onMounted(() => {
+  getCategory();
+})
+
 </script>
 
 <template>
@@ -12,9 +25,8 @@
         <li class="home">
           <RouterLink to="/">首页</RouterLink>
         </li>
-        <li> <RouterLink to="/">居家</RouterLink> </li>
-        <li> <RouterLink to="/">美食</RouterLink> </li>
-        <li> <RouterLink to="/">服饰</RouterLink> </li>
+        <li v-for="item in categoryList" :key="item.id"> <RouterLink to="/">{{ item.name }}</RouterLink> </li>
+        
       </ul>
       <div class="search">
         <i class="iconfont icon-search"></i>
