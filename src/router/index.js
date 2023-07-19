bc54409897557a6f7c90b6eb9d109d1804ef9347
zweirdo <1606403438@qq.com>
2023-07-19 19:53:27 +0800

    二级分类创建组件和路由配置

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import Login from "@/views/Login/index.vue";
 import Layout from "@/views/Layout/index.vue";
 import Category from "@/views/Category/index.vue";
 import Home from "@/views/Home/index.vue";
+import SubCategory from "@/views/SubCategory/index.vue"
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -18,8 +19,13 @@ const router = createRouter({
       {
         // 传入id参数
         path: 'category/:id',
-        component: Category
+        component: Category,
       },
+      {
+          path: 'category/sub/:id',
+          // name: 'subCategory',
+          component: SubCategory
+      }
     ],
     },
     {
