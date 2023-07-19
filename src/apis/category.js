743eeb48dfb8d7f349cf11a4c01f90a59324d1d7
zweirdo <1606403438@qq.com>
2023-07-19 18:35:23 +0800

    category轮播图的实现

--- src/apis/category.js
+++ src/apis/category.js
@@ -1,9 +1,21 @@
 import httpInstance from "@/utils/http";
+
 export function getCategoryAPI(id) {
     return httpInstance({
         url: '/category',
         params: {
             id
+        }
+    })
+}
+
+export function getBannerAPI(params = {}) {
+    // 默认为1, 商品为2 , 这里用解构搞一个默认值
+    const {distributionSite = '1'} = params
+    return httpInstance({
+        url: '/home/banner',
+        params: {
+            distributionSite,
         }
     })
 }
--- src/apis/home.js
+++ src/apis/home.js
@@ -1,9 +1,13 @@
 import httpInstance from "@/utils/http";
 
-export function getBannerAPI() {
+export function getBannerAPI(params = {}) {
+    // 默认为1, 商品为2 , 这里用解构搞一个默认值
+    const {distributionSite = '1'} = params
     return httpInstance({
         url: '/home/banner',
-        // distributionSite: ''
+        params: {
+            distributionSite,
+        }
     })
 }
 
