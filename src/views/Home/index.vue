0947babfdf7c07f157be8be779cc73843722644a
zweirdo <1606403438@qq.com>
2023-07-19 12:39:14 +0800

    home-整体结构拆分和分类模块的数据渲染

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,3 +1,17 @@
+<script setup>
+import HomeCategory from './components/HomeCategory.vue'
+import HomeBanner from './components/HomeBanner.vue'
+import HomeNew from './components/HomeNew.vue'
+import HomeHot from './components/HomeHot.vue'
+import homeProduct from './components/HomeProduct.vue'
+</script>
+
 <template>
-    <div style="height: 1300px;">我是home</div>
+  <div class="container">
+    <HomeCategory />
+    <HomeBanner />
+  </div>
+  <HomeNew />
+  <HomeHot />
+  <homeProduct />
 </template>
