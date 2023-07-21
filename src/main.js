0f1b60ffb107c4ecf44e5cdf043dc3d64dee91a4
zweirdo <1606403438@qq.com>
2023-07-21 13:20:21 +0800

    登录-pinia数据持久化

--- src/main.js
+++ src/main.js
@@ -1,20 +1,22 @@
-import '@/styles/common.scss'
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-
-import App from './App.vue'
-import router from './router'
+import "@/styles/common.scss";
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
+import App from "./App.vue";
+import router from "./router";
 
 // 引入懒加载指令文件并注册
-import { lazyPlugin } from '@/directives' 
+import { lazyPlugin } from "@/directives";
 // 引入全局组件插件
-import {componentPlugin} from '@/components'
-const app = createApp(App)
+import { componentPlugin } from "@/components";
 
-app.use(createPinia())
-app.use(router)
-app.use(lazyPlugin)
-app.use(componentPlugin)
-app.mount('#app')
+const app = createApp(App);
+const pinia = createPinia();
+// 注册持久化插件
+pinia.use(piniaPluginPersistedstate);
 
-
+app.use(pinia); // 注意这里不能用createPinia()了, 要和上面保持同一个pinia实例
+app.use(router);
+app.use(lazyPlugin);
+app.use(componentPlugin);
+app.mount("#app");
--- src/stores/user.js
+++ src/stores/user.js
@@ -4,17 +4,24 @@ import { defineStore } from "pinia";
 import { ref } from "vue";
 import { loginAPI } from "@/apis/user";
 
-export const useUserStore = defineStore("user", () => {
-  // 1. 定义管理数据的state
-  const userInfo = ref({});
-  // 2. 定义获取数据的action函数
-  const getUserInfo = async ({ account, password }) => {
-    const res = await loginAPI({ account, password });
-    userInfo.value = res.result;
-  };
-  // 3. 以对象的格式把state和action return
-  return {
-    userInfo,
-    getUserInfo,
+export const useUserStore = defineStore(
+  "user",
+  () => {
+    // 1. 定义管理数据的state
+    const userInfo = ref({});
+    // 2. 定义获取数据的action函数
+    const getUserInfo = async ({ account, password }) => {
+      const res = await loginAPI({ account, password });
+      userInfo.value = res.result;
+    };
+    // 3. 以对象的格式把state和action return
+    return {
+      userInfo,
+      getUserInfo,
+    };
+  },
+  {
+    // pinia持久缓存 (pinia-plugin-persistedstate插件)
+    persist: true,
   }
-});
+);
